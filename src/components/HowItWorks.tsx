92b26a8efe6d25e8ca33fb2e9e799265869242f4
atheermusa <atheer.musa@zego.com>
2022-04-14 00:27:17 +0100

    remove max width on HowItWorks improves desktop

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -9,7 +9,7 @@ const HowItWorks = () => {
 
   return (
     <section className={`bg-background py-8`} id="howItWorks">
-      <div className={`container max-w-5xl mx-auto m-8`}>
+      <div className={`container mx-auto m-8`}>
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
         >
