90983f23046ad0004475f9f9b756d92c53eadfb6
atheermusa <atheer.musa@zego.com>
2022-04-13 22:17:18 +0100

    update header cta

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,10 +59,10 @@ const Menu = () => {
                 </Link>
               ))}
               <a
-                href="#"
+                href="https://pcmttx0aixz.typeform.com/to/DcqfdFGO"
                 className={`font-medium text-primary hover:text-secondary`}
               >
-                Call to action
+                Sign Up
               </a>
             </div>
           </nav>
