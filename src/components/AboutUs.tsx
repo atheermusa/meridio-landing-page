75c74eb2eb8390aed0cdf761c40c20d2cfa08b2c
atheermusa <atheer.musa@zego.com>
2022-04-13 23:27:24 +0100

    refactor Features into AboutUs

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,21 +2,16 @@ import React from 'react';
 
 import config from '../config/index.json';
 
-const Features = () => {
+const AboutUs = () => {
   const { features } = config;
-  const { title, subtitle, description, items: featuresList } = features;
+  const { title, description, items: featuresList } = features;
   return (
-    <div className={`py-12 bg-background`} id="features">
+    <div className={`py-12 bg-background`} id="aboutUs">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2
-            className={`text-base text-primary font-semibold tracking-wide uppercase`}
-          >
+          <h2 className="mt-2 text-4xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl">
             {title}
           </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            {subtitle}
-          </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
             {description}
           </p>
@@ -25,24 +20,27 @@ const Features = () => {
         <div className="mt-10">
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
             {featuresList.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt>
+              <div key={feature.name} className="relative flex">
                   <div
-                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-primary border-4`}
+                    className={`absolute flex items-center justify-center h-24 w-24 rounded-md bg-background text-tertiary border-primary border-4`}
                   >
                     <img
-                      className={`inline-block h-6 w-6 rounded-full`}
+                      className={`inline-block`}
                       src={feature.icon}
                       alt={feature.name}
                     />
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  <div className='mt-2 ml-16 pl-16'>
+                  <p className="text-xl leading-6 font-medium text-gray-900">
                     {feature.name}
                   </p>
-                </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  <p className="text-base text-gray-400 italic">
+                    Co-founder
+                  </p>
+                <dd className="text-base text-gray-500">
                   {feature.description}
                 </dd>
+                </div>
               </div>
             ))}
           </dl>
@@ -52,4 +50,4 @@ const Features = () => {
   );
 };
 
-export default Features;
+export default AboutUs;
